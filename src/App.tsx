4a84eb5f78bef51ee21b817981279b539ec2810c
Rifat Kudriakov <flungloaf@gmail.com>
2020-10-21 05:10:21 +0300

    Create a material-ui theme and use it

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { CssBaseline, MuiThemeProvider } from "@material-ui/core";
 
 import "./App.css";
 import Home from "./pages/Home";
+import theme from "./theme";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </MuiThemeProvider>
   );
 }
 
