cf0f36cf99ff473216d4476553f68df1d5575deb
MOZGIII <mike-n@narod.ru>
2020-10-25 02:15:50 +0300

    Remove the NODE_PATH mentions

--- config/modules.js
+++ config/modules.js
@@ -13,18 +13,6 @@ const resolve = require("resolve");
  */
 function getAdditionalModulePaths(options = {}) {
   const baseUrl = options.baseUrl;
-
-  // We need to explicitly check for null and undefined (and not a falsy value) because
-  // TypeScript treats an empty string as `.`.
-  if (baseUrl == null) {
-    // If there's no baseUrl set we respect NODE_PATH
-    // Note that NODE_PATH is deprecated and will be removed
-    // in the next major release of create-react-app.
-
-    const nodePath = process.env.NODE_PATH || "";
-    return nodePath.split(path.delimiter).filter(Boolean);
-  }
-
   const baseUrlResolved = path.resolve(paths.appPath, baseUrl);
 
   // We don't need to do anything if `baseUrl` is set to `node_modules`. This is
