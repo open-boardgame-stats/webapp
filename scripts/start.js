c6c05f712a5eaa91867bc77e3bde85a0dd6ad56b
MOZGIII <mike-n@narod.ru>
2020-10-25 02:53:24 +0300

    Remove unused require from scripts/start.js

--- scripts/start.js
+++ scripts/start.js
@@ -11,7 +11,6 @@ process.on("unhandledRejection", (err) => {
   throw err;
 });
 
-const fs = require("fs");
 const chalk = require("react-dev-utils/chalk");
 const webpack = require("webpack");
 const WebpackDevServer = require("webpack-dev-server");
