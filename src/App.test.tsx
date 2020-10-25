6a573254e614c91addaed9a1ee7dbc153b6391f2
Rifat Kudriakov <flungloaf@gmail.com>
2020-10-25 23:53:10 +0300

    Remove tests

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("renders the Hitler message", () => {
-  const { getByText } = render(<App />);
-  const hitlerElement = getByText(/Hitler/i);
-  expect(hitlerElement).toBeInTheDocument();
-});
