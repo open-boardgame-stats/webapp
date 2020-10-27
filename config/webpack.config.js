72cff15dc34cda72a2c30d60c4adccc7c7a22855
MOZGIII <mike-n@narod.ru>
2020-10-27 03:35:21 +0300

    --wip-- [skip ci]

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -405,7 +405,7 @@ module.exports = function (webpackEnv) {
         new WorkboxWebpackPlugin.GenerateSW({
           clientsClaim: true,
           exclude: [/\.map$/, /asset-manifest\.json$/],
-          importWorkboxFrom: "cdn",
+          importWorkboxFrom: "local",
           navigateFallback: paths.publicUrlOrPath + "index.html",
           navigateFallbackBlacklist: [
             // Exclude URLs starting with /_, as they're likely an API call
