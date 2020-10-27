1ef4b68bc5abe6c12a538a8b8da41acac59e157a
MOZGIII <mike-n@narod.ru>
2020-10-27 04:45:55 +0300

    Add bfj support to our build script

--- scripts/build.js
+++ scripts/build.js
@@ -13,6 +13,7 @@ process.on("unhandledRejection", (err) => {
 
 const chalk = require("react-dev-utils/chalk");
 const fs = require("fs-extra");
+const bfj = require("bfj");
 const webpack = require("webpack");
 const configFactory = require("../config/webpack.config");
 const paths = require("../config/paths");
@@ -35,6 +36,9 @@ const isInteractive = process.stdout.isTTY;
 if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
   process.exit(1);
 }
+
+const argv = process.argv.slice(2);
+const writeStatsJson = argv.indexOf("--stats") !== -1;
 
 // Generate configuration
 const config = configFactory("production");
@@ -162,11 +166,20 @@ function build(previousFileSizes) {
         return reject(new Error(messages.warnings.join("\n\n")));
       }
 
-      return resolve({
+      const resolveArgs = {
         stats,
         previousFileSizes,
         warnings: messages.warnings,
-      });
+      };
+
+      if (writeStatsJson) {
+        return bfj
+          .write(paths.appBuild + "/bundle-stats.json", stats.toJson())
+          .then(() => resolve(resolveArgs))
+          .catch((error) => reject(new Error(error)));
+      }
+
+      return resolve(resolveArgs);
     });
   });
 }
