ebe8bac364bfdff0b7194f28d1ce9a7582f56551
Rifat Kudriakov <flungloaf@gmail.com>
2020-11-11 01:43:10 +0300

    Add a context for generic app settings

--- src/App.tsx
+++ src/App.tsx
@@ -14,45 +14,48 @@ import CreateCodenamesMatch from "./pages/games/codenames/Create";
 import TerraformingMars from "./pages/games/terraforming-mars";
 import NotFoundPage from "./pages/NotFoundPage";
 import TerraformingMarsMatch from "./pages/games/terraforming-mars/Match";
+import SettingsProvider from "./settings/Context";
 
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
-      <CssBaseline />
-      <Router>
-        <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/games" exact>
-            <Games />
-          </Route>
-          <Route path="/games/secret_hitler" exact>
-            <SecretHitler />
-          </Route>
-          <Route path="/games/secret_hitler/create">
-            <CreateSecretHitlerMatch />
-          </Route>
-          <Route path="/games/codenames" exact>
-            <Codenames />
-          </Route>
-          <Route path="/games/codenames/create">
-            <CreateCodenamesMatch />
-          </Route>
-          <Route path="/games/terraforming_mars" exact>
-            <TerraformingMars />
-          </Route>
-          <Route path="/games/terraforming_mars/matches/:id">
-            <TerraformingMarsMatch />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="*">
-            <NotFoundPage />
-          </Route>
-        </Switch>
-      </Router>
+      <SettingsProvider>
+        <CssBaseline />
+        <Router>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/games" exact>
+              <Games />
+            </Route>
+            <Route path="/games/secret_hitler" exact>
+              <SecretHitler />
+            </Route>
+            <Route path="/games/secret_hitler/create">
+              <CreateSecretHitlerMatch />
+            </Route>
+            <Route path="/games/codenames" exact>
+              <Codenames />
+            </Route>
+            <Route path="/games/codenames/create">
+              <CreateCodenamesMatch />
+            </Route>
+            <Route path="/games/terraforming_mars" exact>
+              <TerraformingMars />
+            </Route>
+            <Route path="/games/terraforming_mars/matches/:id">
+              <TerraformingMarsMatch />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route path="*">
+              <NotFoundPage />
+            </Route>
+          </Switch>
+        </Router>
+      </SettingsProvider>
     </MuiThemeProvider>
   );
 }
